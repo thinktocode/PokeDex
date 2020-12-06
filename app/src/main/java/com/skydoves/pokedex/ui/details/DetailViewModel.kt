0a19cb58b9ce2c620b367343b2683a9c7cb86e53
thinktocode <73512577+thinktocode@users.noreply.github.com>
2020-12-06 20:07:18 +0530

    Main

---
+++ app/src/main/java/com/skydoves/pokedex/ui/details/DetailViewModel.kt
@@ -0,0 +1,73 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.pokedex.ui.details
+
+import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.skydoves.pokedex.base.LiveCoroutinesViewModel
+import com.skydoves.pokedex.model.PokemonInfo
+import com.skydoves.pokedex.repository.DetailRepository
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import timber.log.Timber
+
+class DetailViewModel @AssistedInject constructor(
+  private val detailRepository: DetailRepository,
+  @Assisted private val pokemonName: String
+) : LiveCoroutinesViewModel() {
+
+  val pokemonInfoLiveData: LiveData<PokemonInfo?>
+
+  private val _toastLiveData: MutableLiveData<String> = MutableLiveData()
+  val toastLiveData: LiveData<String> get() = _toastLiveData
+
+  val isLoading: ObservableBoolean = ObservableBoolean(false)
+
+  init {
+    Timber.d("init DetailViewModel")
+
+    pokemonInfoLiveData = launchOnViewModelScope {
+      isLoading.set(true)
+      detailRepository.fetchPokemonInfo(
+        name = pokemonName,
+        onSuccess = { isLoading.set(false) },
+        onError = { _toastLiveData.postValue(it) }
+      ).asLiveData()
+    }
+  }
+
+  @AssistedInject.Factory
+  interface AssistedFactory {
+    fun create(pokemonName: String): DetailViewModel
+  }
+
+  companion object {
+    fun provideFactory(
+      assistedFactory: AssistedFactory,
+      pokemonName: String
+    ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+      @Suppress("UNCHECKED_CAST")
+      override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return assistedFactory.create(pokemonName) as T
+      }
+    }
+  }
+}
---
+++ app/src/test/java/com/skydoves/pokedex/viewmodel/DetailViewModelTest.kt
@@ -0,0 +1,82 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.pokedex.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
+import com.nhaarman.mockitokotlin2.atLeastOnce
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.skydoves.pokedex.MainCoroutinesRule
+import com.skydoves.pokedex.model.PokemonInfo
+import com.skydoves.pokedex.network.PokedexClient
+import com.skydoves.pokedex.network.PokedexService
+import com.skydoves.pokedex.persistence.PokemonInfoDao
+import com.skydoves.pokedex.repository.DetailRepository
+import com.skydoves.pokedex.ui.details.DetailViewModel
+import com.skydoves.pokedex.utils.MockUtil
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class DetailViewModelTest {
+
+  private lateinit var viewModel: DetailViewModel
+  private lateinit var detailRepository: DetailRepository
+  private val pokedexService: PokedexService = mock()
+  private val pokdexClient: PokedexClient = PokedexClient(pokedexService)
+  private val pokemonInfoDao: PokemonInfoDao = mock()
+
+  @ExperimentalCoroutinesApi
+  @get:Rule
+  var coroutinesRule = MainCoroutinesRule()
+
+  @get:Rule
+  var instantExecutorRule = InstantTaskExecutorRule()
+
+  @ExperimentalCoroutinesApi
+  @Before
+  fun setup() {
+    detailRepository = DetailRepository(pokdexClient, pokemonInfoDao)
+    viewModel = DetailViewModel(detailRepository, "skydoves")
+  }
+
+  @Test
+  fun fetchPokemonInfoTest() = runBlocking {
+    val mockData = MockUtil.mockPokemonInfo()
+    whenever(pokemonInfoDao.getPokemonInfo(name_ = "skydoves")).thenReturn(mockData)
+
+    val observer: Observer<PokemonInfo?> = mock()
+    val fetchedData: LiveData<PokemonInfo?> =
+      detailRepository.fetchPokemonInfo(
+        name = "skydoves",
+        onSuccess = {},
+        onError = {}
+      ).asLiveData()
+    fetchedData.observeForever(observer)
+
+    verify(pokemonInfoDao, atLeastOnce()).getPokemonInfo(name_ = "skydoves")
+    verify(observer).onChanged(mockData)
+    fetchedData.removeObserver(observer)
+  }
+}
