ffd70b2255b97f5676377bb7e24f7f7215d6527b
thinktocode <73512577+thinktocode@users.noreply.github.com>
2020-12-16 19:24:54 +0530

    NEW

--- app/src/main/java/com/namanbir/pokedex/namanbir/pokedex/PokedexApp.kt
+++
@@ -1,9 +0,0 @@
-
-
-package com.namanbir.pokedex.namanbir.pokedex
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PokedexApp : Application()
--- app/src/main/java/com/namanbir/pokedex/namanbir/pokedex/repository/Repository.kt
+++
@@ -1,6 +0,0 @@
-
-
-package com.namanbir.pokedex.namanbir.pokedex.repository
-
-/** Repository is an interface for configuring base repository classes. */
-interface Repository
