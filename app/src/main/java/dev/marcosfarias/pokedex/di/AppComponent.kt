80d594e2ff456f898ba788a5d775214b716de58f
thinktocode <73512577+thinktocode@users.noreply.github.com>
2020-12-20 11:09:25 +0530

    Delete

--- app/src/main/java/dev/marcosfarias/pokedex/di/AppComponent.kt
+++
@@ -1,7 +0,0 @@
-package dev.marcosfarias.pokedex.di
-
-val appComponent = listOf(
-    databaseModule,
-    networkModule,
-    viewModelsModule
-)
--- app/src/main/java/dev/marcosfarias/pokedex/model/Menu.kt
+++
@@ -1,7 +0,0 @@
-package dev.marcosfarias.pokedex.model
-
-data class Menu(
-    val id: Int,
-    val name: String,
-    val color: Int
-)
